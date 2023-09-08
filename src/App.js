c6a19ff609517d3937eb6eaf1cbcb7152bb2a1ee
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2023-09-08 08:28:47 +0300

    A new component Library is created. Functionalities have been added to it like switching songs,selected from the Library and rendered on the Player component. Library component is styled with scss file - _library.css from folder 'styles'.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './styles/app.scss'
 import React, { useState } from "react";
-
 import Song from './components/Song'
 import Player from './components/Player'
 import data from './util'
+import Library from './components/Library';
 
 
 
@@ -19,6 +19,7 @@ const [isPlaying, setIsPlaying] = useState(false);
     <div className="App">
       <Song currentSong={currentSong}  />
       <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
+      <Library songs={songs} setCurrentSong={setCurrentSong}  />
     </div>
   );
 }
